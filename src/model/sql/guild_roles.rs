f49179a2810678208f5825cb65085604267fbc3e
drklee3 <derrick@dlee.dev>
2020-08-21 12:08:07 -0700

    fix: Remove GuildRole search

--- src/model/sql/guild_roles.rs
+++ src/model/sql/guild_roles.rs
@@ -3,9 +3,12 @@ use std::collections::HashMap;
 
 #[derive(Deserialize, Default, Serialize, Clone, Debug)]
 pub struct GuildRole {
-    pub search: String,
-    pub primary: u64,
-    pub secondary: Option<u64>,
+    /// Main role id that has the highest priority for the colour
+    pub primary_id: u64,
+
+    /// Secondary role that is lower in priority than **all** other primary roles
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub secondary_id: Option<u64>,
 }
 
 #[derive(Deserialize, Default, Serialize, Clone, Debug)]
