dd46fab5349f4c05677009cd13500970a3bce86a
drklee3 <derrick@dlee.dev>
2020-09-16 22:55:35 -0700

    fix: Add manage roles to permissions

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -13,7 +13,7 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 #[description("Gets the invite link for sushii")]
 async fn invite(ctx: &Context, msg: &Message) -> CommandResult {
     // TODO: Pass invite link via config
-    msg.channel_id.say(&ctx.http, "https://discord.com/api/oauth2/authorize?client_id=249784936318369793&permissions=388166&scope=bot").await?;
+    msg.channel_id.say(&ctx.http, "https://discord.com/api/oauth2/authorize?client_id=249784936318369793&permissions=268823622&scope=bot").await?;
 
     Ok(())
 }
