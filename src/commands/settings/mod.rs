61644d9ac8fd71021163377959ea8caafa71fe59
drklee3 <derrick@dlee.dev>
2020-09-17 13:25:34 -0700

    feat: Send help for settings group default

--- src/commands/settings/mod.rs
+++ src/commands/settings/mod.rs
@@ -3,13 +3,15 @@ use serenity::framework::standard::macros::group;
 mod list;
 mod mod_log;
 mod mute_role;
+mod default;
 
-use self::{list::*, mod_log::*, mute_role::*};
+use self::{list::*, mod_log::*, mute_role::*, default::*};
 
 #[group]
 #[commands(list, modlog, muterole)]
 #[description("Guild settings, requires MANAGE_GUILD permissions")]
-#[prefix = "settings"]
+#[prefix("settings")]
 #[only_in("guild")]
+#[default_command(default)]
 #[required_permissions("MANAGE_GUILD")]
 pub struct Settings;
