6eed8d1137d9b1800ee4cdaa03339bb0bfd52722
drklee3 <derrick@dlee.dev>
2020-09-20 17:08:06 -0700

    fix: Update setchannel cmd

--- src/commands/roles/mod.rs
+++ src/commands/roles/mod.rs
@@ -8,7 +8,7 @@ pub mod set_roles_channel;
 use self::{get_roles::*, list_role_ids::*, set_roles::*, set_roles_channel::*};
 
 #[group]
-#[commands(set, get, set_channel, listids)]
+#[commands(set, get, setchannel, listids)]
 #[prefix = "roles"]
 #[only_in("guild")]
 #[required_permissions("MANAGE_GUILD")]
