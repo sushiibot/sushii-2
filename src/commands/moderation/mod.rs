9dbc98d7fb8138590d60d55f03dfd2d5ede28ef8
drklee3 <derrick@dlee.dev>
2020-09-28 15:37:42 -0700

    fix: Add reason command to mod cmd group

--- src/commands/moderation/mod.rs
+++ src/commands/moderation/mod.rs
@@ -4,12 +4,12 @@ pub mod cases;
 pub mod chat;
 
 use self::{
-    cases::{ban::*, history::*, kick::*, mute::*, warn::*},
+    cases::{ban::*, history::*, kick::*, mute::*, warn::*, reason::*},
     chat::*,
 };
 
 #[group]
-#[commands(prune, history, ban, unban, kick, mute, unmute, warn)]
+#[commands(prune, history, ban, unban, kick, mute, reason, unmute, warn)]
 #[only_in("guild")]
 #[required_permissions("BAN_MEMBERS")]
 pub struct Moderation;
