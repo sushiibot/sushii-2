12f01d255ca66c7a738701a81b39e9e2309a9846
drklee3 <derrick@dlee.dev>
2020-12-08 13:39:04 -0800

    fix: Add tag delete to Tag command group

--- src/commands/tags/mod.rs
+++ src/commands/tags/mod.rs
@@ -11,7 +11,7 @@ mod top;
 use self::{add::*, edit::*, get::*, info::*, list::*, search::*, top::*};
 
 #[group]
-#[commands(add, edit, info, get, rename, random, list, search, top)]
+#[commands(add, edit, delete, info, get, rename, random, list, search, top)]
 #[only_in("guild")]
 #[prefixes("tag", "t")]
 #[default_command(get)]
