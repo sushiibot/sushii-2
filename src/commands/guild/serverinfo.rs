1c1a6eb0d99b19e452b19e03a827fcdad7caf057
drklee3 <derrick@dlee.dev>
2020-12-17 17:51:50 -0800

    feat: Add more details to serverinfo and make more compact

--- src/commands/guild/serverinfo.rs
+++ src/commands/guild/serverinfo.rs
@@ -1,8 +1,11 @@
+use num_format::{Locale, ToFormattedString};
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use std::fmt::Write;
 
 #[command]
+#[aliases("guildinfo")]
 #[only_in("guild")]
 async fn serverinfo(ctx: &Context, msg: &Message) -> CommandResult {
     let guild = match msg.guild(ctx).await {
@@ -16,11 +19,43 @@ async fn serverinfo(ctx: &Context, msg: &Message) -> CommandResult {
 
     let owner = guild.owner_id.to_user(ctx).await?;
 
-    let features_str = if !guild.features.is_empty() {
-        guild.features.join(", ")
-    } else {
-        "None".into()
-    };
+    let mut guild_str = String::new();
+
+    writeln!(guild_str, "**Owner:** {} (ID {})", owner.tag(), owner.id.0)?;
+    writeln!(guild_str, "**Created:** {}", guild.id.created_at().format("%Y-%m-%d %H:%M:%S"))?;
+    writeln!(guild_str, "**Members:** {}", guild.member_count.to_formatted_string(&Locale::en))?;
+    writeln!(guild_str, "**Region:** {}", guild.region)?;
+    writeln!(guild_str, "**Roles:** {}", guild.roles.len())?;
+    writeln!(guild_str, "**Verification Level:** {:?}", guild.verification_level)?;
+    writeln!(guild_str, "**Explicit Content Filter:** {:?}", guild.explicit_content_filter)?;
+
+    if !guild.features.is_empty() {
+        writeln!(guild_str, "**Features:** {}", guild.features.join(", "))?;
+    }
+
+    let (text_channels, voice_channels) = guild.channels.values().fold((0, 0), |mut acc, chan| {
+        if chan.kind == ChannelType::Text {
+            acc.0 += 1;
+        } else if chan.kind == ChannelType::Voice {
+            acc.1 += 1;
+        };
+
+        acc
+    });
+
+    writeln!(guild_str, "**Channels:** {} text, {} voice", text_channels, voice_channels)?;
+
+    let (emojis, animated_emojis) = guild.emojis.values().fold((0, 0), |mut acc, emoji| {
+        if emoji.animated {
+            acc.1 += 1;
+        } else {
+            acc.0 += 1;
+        }
+
+        acc
+    });
+
+    writeln!(guild_str, "**Emojis:** {} static, {} animated", emojis, animated_emojis)?;
 
     msg.channel_id
         .send_message(ctx, |m| {
@@ -39,32 +74,8 @@ async fn serverinfo(ctx: &Context, msg: &Message) -> CommandResult {
                     e.thumbnail(url);
                 }
 
-                e.field(
-                    "Owner",
-                    &format!("{} (ID {})", owner.tag(), owner.id.0),
-                    true,
-                );
-                e.field("Features", features_str, true);
-                e.field("Channels", &guild.channels.len().to_string(), true);
-                e.field("Emojis", &guild.emojis.len().to_string(), true);
-                e.field(
-                    "Explicit Content Filter",
-                    &format!("{:?}", guild.explicit_content_filter),
-                    true,
-                );
-                e.field("Member Count", &guild.member_count.to_string(), true);
-                e.field("Region", &guild.region, true);
-                e.field("Roles", &guild.roles.len().to_string(), true);
-                e.field(
-                    "Verification Level",
-                    &format!("{:?}", guild.verification_level),
-                    true,
-                );
-                e.field(
-                    "Created At",
-                    guild.id.created_at().format("%Y-%m-%dT%H:%M:%S"),
-                    false,
-                );
+                e.description(guild_str);
+
                 e.footer(|f| {
                     f.text(&format!("Guild ID: {}", &guild.id.0));
 
