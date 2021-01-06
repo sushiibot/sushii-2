21cc4484d274c934463d432a5ef4b33c8d606a4a
drklee3 <derrick@dlee.dev>
2021-01-06 14:36:17 -0800

    feat(sushii): Allow slowmode for other channels

--- sushii-2/src/commands/moderation/channel.rs
+++ sushii-2/src/commands/moderation/channel.rs
@@ -1,6 +1,7 @@
 use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use serenity::utils::parse_channel;
 
 #[command]
 #[aliases("sm")]
@@ -9,16 +10,48 @@ async fn slowmode(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
     let slowmode_rate = match args.single::<u64>() {
         Ok(n) => n,
         Err(_) => {
-            msg.channel_id
-                .say(
-                    ctx,
-                    "Error: Invalid slowmode seconds, must be between 0 and 120 (inclusive)",
-                )
-                .await?;
+            msg.reply(
+                ctx,
+                "Error: Invalid slowmode seconds, must be between 0 and 120 (inclusive)",
+            )
+            .await?;
 
             return Ok(());
         }
     };
+
+    let target_channel_str = args.rest().trim();
+    let target_channel = if target_channel_str.is_empty() {
+        msg.channel_id
+    } else {
+        match target_channel_str
+            .parse::<u64>()
+            .ok()
+            .or_else(|| parse_channel(target_channel_str))
+        {
+            Some(id) => ChannelId(id),
+            None => {
+                msg.reply(ctx, "Error: Invalid channel").await?;
+
+                return Ok(());
+            }
+        }
+    };
+
+    let guild = match msg.guild(ctx).await {
+        Some(g) => g,
+        None => {
+            msg.reply(ctx, "No guild found").await?;
+
+            return Ok(());
+        }
+    };
+
+    if !guild.channels.contains_key(&target_channel) {
+        msg.reply(ctx, "Error: Channel is not in this guild or could not be found").await?;
+
+        return Ok(());
+    }
 
     if slowmode_rate > 120 {
         msg.channel_id
@@ -31,7 +64,7 @@ async fn slowmode(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
         return Ok(());
     }
 
-    msg.channel_id
+    target_channel
         .edit(ctx, |c| c.slow_mode_rate(slowmode_rate))
         .await?;
 
@@ -40,7 +73,7 @@ async fn slowmode(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
             ctx,
             format!(
                 "Updated slowmode in {} to {} seconds",
-                msg.channel_id.mention(),
+                target_channel.mention(),
                 slowmode_rate
             ),
         )
