928b9eced99ba1cf3a6649d45fa2679d6c5a291e
drklee3 <derrick@dlee.dev>
2021-01-30 12:12:54 -0800

    feat(feeds): Add vlive author icon, live/vod, duration

--- sushii-feeds/src/update.rs
+++ sushii-feeds/src/update.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use sushii_model::model::sql::{Feed, FeedMetadata};
+use std::time::Duration;
 use vlive::VLiveRequester;
 
 use sushii_feeds::feed_request::feed_update_reply::{Author, FeedItem as GrpcFeedItem, Post};
@@ -28,22 +29,55 @@ pub async fn update_vlive(ctx: Context) -> Result<Vec<GrpcFeedItem>> {
     let mut grpc_items = Vec::new();
 
     for video in videos {
-        // let video_data = ctx.client.get_video(video.video_seq).await?;
+        let video_data = ctx.client.get_video(video.video_seq).await;
+
+        if let Err(ref e) = video_data {
+            tracing::warn!("Failed to fetch video data: {}", e);
+        }
+
         tracing::info!("video: {:?}", &video);
 
         let feed_id = format!("vlive:videos:{}", video.channel_code);
 
+        // Author icon
+        let icon = if let Ok(ref data) = video_data {
+            data.channel.channel.channel_profile_image.clone()
+        } else {
+            "https://i.imgur.com/NzGrmho.jpg".to_string()
+        };
+
+        // If live or vod
+        let title = if video.duration_secs.is_some() {
+            format!("[LIVE] {}", video.title)
+        } else {
+            format!("[VOD] {}", video.title)
+        };
+
+        let description = if let Some(secs) = video.duration_secs {
+            let d = Duration::from_secs(secs);
+            format!("Duration: {}", humantime::format_duration(d))
+        } else {
+            "".to_string()
+        };
+
+        let color = video_data
+            .ok()
+            .map(|d| d.channel.channel.representative_color)
+            .and_then(|c| u32::from_str_radix(&c[1..], 16).ok())
+            .unwrap_or(0x1ecfff);
+
         let grpc_post = Post {
             id: video.video_url(),
-            title: video.title.clone(),
+            title,
             author: Some(Author {
                 name: video.channel_name.clone(),
                 url: video.channel_url(),
-                icon: "".into(),
+                icon,
             }),
-            description: "New video".to_string(),
+            description,
             thumbnail: video.thumbnail_url.clone().unwrap_or_else(|| "".into()),
             url: video.video_url(),
+            color,
         };
 
         let grpc_item = GrpcFeedItem {
