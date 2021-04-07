4ebcb2d528f09941ef17badf3507f755fd866349
drklee3 <derrick@dlee.dev>
2021-04-07 11:28:48 -0700

    fix(sushii): Update mention/invite response text to prevent embed

--- sushii-2/src/commands/meta.rs
+++ sushii-2/src/commands/meta.rs
@@ -21,7 +21,8 @@ async fn invite(ctx: &Context, msg: &Message) -> CommandResult {
     // TODO: Pass invite link via config
     msg.channel_id.say(
         ctx,
-        "https://discord.com/oauth2/authorize?client_id=193163942502072320&permissions=268823622&scope=applications.commands%20bot"
+        "You can invite sushii with this link! \n\
+        <https://discord.com/oauth2/authorize?client_id=193163942502072320&permissions=268823622&scope=applications.commands%20bot>"
     ).await?;
 
     Ok(())
