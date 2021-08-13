e7fa1f2f19d8e0a9916339d882dcc9b3ce57bd64
drklee3 <derrick@dlee.dev>
2021-08-12 22:52:44 -0700

    feat(sushii): Add banner command

---
+++ sushii-2/src/commands/users/banner.rs
@@ -0,0 +1,69 @@
+use serenity::framework::standard::{macros::command, Args, CommandResult};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+
+use crate::utils::user::parse_id;
+
+#[command]
+async fn banner(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let target_str = args.rest();
+
+    let target_id = match parse_id(target_str) {
+        Some(id) => UserId(id),
+        None => {
+            if !target_str.is_empty() {
+                msg.channel_id
+                    .say(ctx, "Error: Invalid user given.")
+                    .await?;
+
+                return Ok(());
+            }
+
+            // If empty use self
+            msg.author.id
+        }
+    };
+
+    let user = match target_id.to_user(&ctx).await {
+        Ok(u) => u,
+        Err(_) => {
+            msg.reply(
+                &ctx,
+                "Error: Failed to fetch user, are you using a correct user ID?",
+            )
+            .await?;
+
+            return Ok(());
+        }
+    };
+
+    let banner_url = match user.banner_url() {
+        Some(url) => url,
+        None => {
+            msg.channel_id.say(&ctx, "User has no banner set.").await?;
+
+            return Ok(());
+        }
+    };
+
+    msg.channel_id
+        .send_message(ctx, |m| {
+            m.embed(|e| {
+                e.title(format!("{} banner", user.tag()));
+                e.url(&banner_url);
+
+                e.image(&banner_url);
+
+                if let Some(accent_color) = user.accent_color {
+                    e.footer(|f| f.text(format!("Accent color #{}", accent_color.hex())));
+                }
+
+                e
+            });
+
+            m
+        })
+        .await?;
+
+    Ok(())
+}
--- sushii-2/src/commands/users/mod.rs
+++ sushii-2/src/commands/users/mod.rs
@@ -1,15 +1,16 @@
 use serenity::framework::standard::macros::group;
 
 mod avatar;
+mod banner;
 mod fishy;
 mod hug;
 mod rank;
 mod rep;
 mod userinfo;
 
-use self::{avatar::*, fishy::*, hug::*, rank::*, rep::*, userinfo::*};
+use self::{avatar::*, banner::*, fishy::*, hug::*, rank::*, rep::*, userinfo::*};
 
 #[group]
-#[commands(avatar, rank, rep, fishy, userinfo, hug)]
+#[commands(avatar, banner, rank, rep, fishy, userinfo, hug)]
 #[only_in("guild")]
 pub struct Users;
