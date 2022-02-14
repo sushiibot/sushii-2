71b36c7126f1545d0fc18361bd0fb6fc343f9cef
drklee3 <derrick@dlee.dev>
2022-02-13 20:53:03 -0800

    Add privacy disclaimer to help command

--- sushii-2/src/commands/help.rs
+++ sushii-2/src/commands/help.rs
@@ -5,11 +5,15 @@ use serenity::prelude::*;
 #[command]
 async fn help(ctx: &Context, msg: &Message) -> CommandResult {
     msg.channel_id
-        .say(
-            &ctx,
-            "You can find a list of commands here: <https://sushii.xyz/commands>\n\
-            Join the support server if you still have questions: https://discord.gg/Bz5Q2WfuE7",
-        )
+        .send_message(ctx, |m| {
+            m.embed(|e| {
+                e.description(
+                    "You can find a list of commands here: <https://sushii.xyz/commands>\n\
+                    Join the support server if you still have questions: https://discord.gg/Bz5Q2WfuE7\n\n\
+                    **Please note: The server moderators may choose to log your messages and you can opt out by leaving the server.**",
+                )
+            })
+        })
         .await?;
 
     Ok(())
