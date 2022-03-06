edc0cbb724a8be02c0e4af08dada20ed15682d6a
drklee3 <derrick@dlee.dev>
2022-03-06 10:37:08 -0800

    Remove help link embed hiding

--- sushii-2/src/commands/help.rs
+++ sushii-2/src/commands/help.rs
@@ -8,7 +8,7 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
         .send_message(ctx, |m| {
             m.embed(|e| {
                 e.description(
-                    "You can find a list of commands here: <https://sushii.xyz/commands>\n\
+                    "You can find a list of commands here: https://sushii.xyz/commands\n\
                     Join the support server if you still have questions: https://discord.gg/Bz5Q2WfuE7\n\n\
                     **Please note: The server moderators may choose to log your messages and you can opt out by leaving the server.**",
                 )
