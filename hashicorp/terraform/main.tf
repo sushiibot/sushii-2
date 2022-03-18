bf5841f4e1318a77fef9bc7cfea2fb047bad7fee
drklee3 <derrick@dlee.dev>
2022-03-17 19:27:27 -0700

    Update ansible conf

--- hashicorp/terraform/main.tf
+++ hashicorp/terraform/main.tf
@@ -4,23 +4,19 @@ terraform {
       source = "hetznercloud/hcloud"
       version = "1.26.0"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.3.0"
+    }
   }
 }
 
 # Set the variable value in *.tfvars file
 # or using the -var="hcloud_token=..." CLI option
 variable "hcloud_token" {}
-variable "ssh_priv_key" {
-  type    = string
-  default = "~/.ssh/id_rsa"
-}
 variable "ssh_pub_key" {
   type    = string
   default = "~/.ssh/id_rsa.pub"
-}
-variable "cloud_init_script" {
-  type    = string
-  default = "cloud-init.yaml"
 }
 
 ########################################
@@ -35,58 +31,33 @@ resource "hcloud_ssh_key" "default" {
   public_key = file(var.ssh_pub_key)
 }
 
-resource "hcloud_network" "network" {
-  name     = "network"
-  ip_range = "10.0.0.0/16"
+module "hcloud_kubernetes_cluster" {
+  source          = "JWDobken/kubernetes/hcloud"
+  cluster_name    = "demo-cluster"
+  hcloud_token    = var.hcloud_token
+  hcloud_ssh_keys = [hcloud_ssh_key.default.id]
+  master_type     = "cx11" # optional
+  worker_type     = "cx11" # optional
+  worker_count    = 2
 }
 
-resource "hcloud_network_subnet" "network-subnet" {
-  type         = "cloud"
-  network_id   = hcloud_network.network.id
-  network_zone = "eu-central"
-  ip_range     = "10.0.1.0/24"
+output "kubeconfig" {
+  value = module.hcloud_kubernetes_cluster.kubeconfig
 }
 
-# cloud-init script
-data "template_file" "user_data" {
-  template = file(var.cloud_init_script)
-}
+provider "helm" {
+  kubernetes {
+    host     = module.hcloud_kubernetes_cluster.endpoint
 
-resource "hcloud_server" "hashi_node" {
-  name        = "node1-hashi"
-  server_type = "cx11"
-  image       = "ubuntu-20.04"
-  location    = "nbg1"
-  ssh_keys    = [hcloud_ssh_key.default.id]
-  user_data   = data.template_file.user_data
-
-  network {
-    network_id = hcloud_network.network.id
-    ip         = "10.0.1.1"
+    cluster_ca_certificate = base64decode(module.hcloud_kubernetes_cluster.certificate_authority_data)
+    client_certificate     = base64decode(module.hcloud_kubernetes_cluster.client_certificate_data)
+    client_key             = base64decode(module.hcloud_kubernetes_cluster.client_key_data)
   }
-
-  # **Note**: the depends_on is important when directly attaching the
-  # server to a network. Otherwise Terraform will attempt to create
-  # server and sub-network in parallel. This may result in the server
-  # creation failing randomly.
-  depends_on = [
-    hcloud_network_subnet.network-subnet
-  ]
 }
 
-resource "hcloud_server" "sushii_web" {
-  name        = "node2-web"
-  server_type = "cx11"
-  image       = "ubuntu-20.04"
-  location    = "nbg1"
-  ssh_keys    = [hcloud_ssh_key.default.id]
+resource "helm_release" "kube_prometheus_stack" {
+  name       = "kube-prometheus-stack"
 
-  network {
-    network_id = hcloud_network.network.id
-    ip         = "10.0.1.2"
-  }
-
-  depends_on = [
-    hcloud_network_subnet.network-subnet
-  ]
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus-community/kube-prometheus-stack"
 }
