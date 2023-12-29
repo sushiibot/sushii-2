fde2455067f4e1c8afcf8ab8da2323d2a5df7bd4
drklee3 <derrick@dlee.dev>
2023-12-28 21:40:17 -0800

    Disable cache handlers

--- sushii-2/src/handlers/mod.rs
+++ sushii-2/src/handlers/mod.rs
@@ -26,7 +26,7 @@ impl EventHandler for Handler {
         // These both only run once even if ready is called multiple times
         // This is here instead of cache_ready as a single unavailable guild will
         // prevent any of it from starting
-        tasks::start(&ctx).await;
+        // tasks::start(&ctx).await;
 
         // Disabled ban fetching, now handled in sushii-ts-services
         // bans::start(&ctx, ready.guilds.iter().map(|g| g.id).collect::<Vec<_>>()).await;
@@ -39,105 +39,5 @@ impl EventHandler for Handler {
     async fn resume(&self, ctx: Context, _: ResumedEvent) {
         tracing::info!("Resumed");
         ctx.set_activity(Activity::playing("sushii.xyz")).await;
-    }
-
-    async fn message(&self, ctx: Context, msg: Message) {
-        tokio::join!(
-            roles::message(&ctx, &msg),
-            // Disable user levels handler, now handled in sushii-ts-services
-            // user_levels::message(&ctx, &msg),
-
-            // Message logs handled in sushii-ts-services
-            // msg_log::message(&ctx, &msg),
-            // mention::message(&ctx, &msg),
-            cache::cache_user::message(&ctx, &msg),
-            // notification::message(&ctx, &msg),
-        );
-    }
-
-    async fn message_delete(
-        &self,
-        _ctx: Context,
-        _channel_id: ChannelId,
-        _msg_id: MessageId,
-        _guild_id: Option<GuildId>,
-    ) {
-        // Handled in sushii-ts-services
-        // msg_log::message_delete(&ctx, channel_id, msg_id, guild_id).await;
-    }
-
-    async fn message_delete_bulk(
-        &self,
-        _ctx: Context,
-        _channel_id: ChannelId,
-        _msg_ids: Vec<MessageId>,
-        _guild_id: Option<GuildId>,
-    ) {
-        // Handled in sushii-ts-services
-        // msg_log::message_delete_bulk(&ctx, channel_id, msg_ids, guild_id).await;
-    }
-
-    async fn message_update(
-        &self,
-        _ctx: Context,
-        _old_msg: Option<Message>,
-        _new_msg: Option<Message>,
-        _event: MessageUpdateEvent,
-    ) {
-        // Handled in sushii-ts-services
-        // msg_log::message_update(&ctx, &old_msg, &new_msg, &event).await;
-    }
-
-    async fn guild_ban_addition(&self, ctx: Context, guild_id: GuildId, banned_user: User) {
-        // Disabled ban fetching, now handled in sushii-ts-services
-        // bans::guild_ban_addition(&ctx, guild_id, &banned_user).await;
-    }
-
-    async fn guild_ban_removal(&self, ctx: Context, guild_id: GuildId, unbanned_user: User) {
-        // Disabled ban fetching, now handled in sushii-ts-services
-        // bans::guild_ban_removal(&ctx, guild_id, &unbanned_user).await;
-    }
-
-    async fn guild_create(&self, ctx: Context, guild: Guild, is_new: bool) {
-        cache::cache_guild::guild_create(&ctx, &guild, is_new).await;
-        // bans::guild_create(&ctx, &guild, is_new).await;
-    }
-
-    async fn guild_update(
-        &self,
-        ctx: Context,
-        old_guild_if_avail: Option<Guild>,
-        partial_guild: PartialGuild,
-    ) {
-        cache::cache_guild::guild_update(&ctx, &old_guild_if_avail, &partial_guild).await;
-    }
-
-    async fn guild_member_update(
-        &self,
-        ctx: Context,
-        old_member: Option<Member>,
-        new_member: Member,
-    ) {
-    }
-
-    async fn guild_member_addition(&self, ctx: Context, mut member: Member) {
-        tokio::join!(
-            // join_msg::guild_member_addition(&ctx, &member.guild_id, &member),
-            // member_log::guild_member_addition(&ctx, &member.guild_id, &member),
-        );
-    }
-
-    async fn guild_member_removal(
-        &self,
-        ctx: Context,
-        guild_id: GuildId,
-        user: User,
-        member: Option<Member>,
-    ) {
-        // member_log::guild_member_removal(&ctx, &guild_id, &user, &member).await;
-    }
-
-    async fn interaction_create(&self, _ctx: Context, interaction: Interaction) {
-        tracing::debug!(?interaction, "Received interaction");
     }
 }
